69a874c249546b755ef47c0dd143087e94fedb47
xhanin <xavier.hanin@4sh.fr>
2014-02-23 21:43:03 +0100

    add ui module
    based on restx app new / angular-bootstrap-grunt-bower

---
+++ ui/app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('rxinvoiceApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'RESTX',
+      'Twitter Bootstrap',
+      'AngularJS',
+      'Karma'
+    ];
+  });
